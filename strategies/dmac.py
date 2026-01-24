9f81c869b313536f930a0e96bcea8d0bd288783e
Ruben <Ruben.kelchtermans9@gmail.com>
2026-01-24 22:04:56 +0100

    remade DMAC logic

--- strategies/dmac.py
+++ strategies/dmac.py
@@ -1,10 +1,1 @@
-import pandas as pd
-import os
-from pathlib import Path
-import backtrader as bt
-
-PROJECT_ROOT = Path.cwd().parent
-data_dir = Path(f"{PROJECT_ROOT}/data/raw")
-files = list(data_dir.glob("*"))
-
-print(files)
+# this file does not test strategys but defines it
