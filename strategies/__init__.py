9cf779d4afcaae86c1bf77cb765f5bce5166d069
Ruben <Ruben.kelchtermans9@gmail.com>
2026-01-28 17:31:54 +0100

    changed logic

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,7 +1,9 @@
 import backtrader as bt
 import pandas as pd
-from .dmac_strat import run as dmac  
+from .dmac_strat import run as dmac
+from .rsi_ma import run as rsi_ma
 
 ALL_STRATEGIES = {
-    "DMAC": dmac
+    "run_dmac": dmac,
+    "run_rsi_ma": rsi_ma
 }
