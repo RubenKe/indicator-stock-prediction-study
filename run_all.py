bd9e396223a084fe3fec7cd9279ca14bcd0a096b
Ruben <Ruben.kelchtermans9@gmail.com>
2026-01-28 17:32:57 +0100

    Added first strategy run

--- run_all.py
+++ run_all.py
@@ -3,10 +3,29 @@ import datetime
 import pandas as pd
 from strategies import ALL_STRATEGIES
 import yaml
+import os
+from pathlib import Path
 
-# open the config
+# make the path with data
+DATA_PROCESSED = Path("data") / "raw"
+
+# Open the config
 with open("config/config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
+# Load config
 commission = config['commission']
 sizer = config['sizer']
+stocks = config['stocks']
+forex = config['forex']
+indices = config['indices']
+intervals = config['intervals']
+params = config['params']
+
+
+
+df = pd.read_csv(DATA_PROCESSED / "AAPL_1d.csv", index_col=0, parse_dates=True)
+data = bt.feeds.PandasData(dataname=df)
+
+strat = ALL_STRATEGIES['run_dmac'](data, commission, sizer)
+print(strat.broker.getvalue())
