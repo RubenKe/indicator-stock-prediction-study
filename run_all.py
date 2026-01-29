e819982e25c3ee93adf4290c036c6adaac335dd6
Ruben <Ruben.kelchtermans9@gmail.com>
2026-01-29 15:01:17 +0100

    Prepared strategies and param sets for main loop

--- run_all.py
+++ run_all.py
@@ -1,3 +1,4 @@
+from tkinter import ALL
 import backtrader as bt
 import datetime
 import pandas as pd
@@ -5,6 +6,8 @@ from strategies import ALL_STRATEGIES
 import yaml
 import os
 from pathlib import Path
+from itertools import product
+
 
 # make the path with data
 DATA_PROCESSED = Path("data") / "raw"
@@ -22,10 +25,23 @@ indices = config['indices']
 intervals = config['intervals']
 params = config['params']
 
+# Setting configs
+all_symbols = stocks + forex + indices
+strategy_names = list(params.keys())
+param_sets = {
+    "DMAC": list(product(params["DMAC"]["short_ema"], params["DMAC"]["long_ema"])),
+    "RSI_MA": list(product(params["RSI_MA"]["ma"], params["RSI_MA"]["buy_rsi"], params["RSI_MA"]["sell_rsi"]))
+}
 
 
-df = pd.read_csv(DATA_PROCESSED / "AAPL_1d.csv", index_col=0, parse_dates=True)
+
+"""
+# Main loop
+for symbol in all_symbols:
+    for interval in intervals:
+
+
+df = pd.read_csv(DATA_PROCESSED / f'{symbol}_{interval}.csv', index_col=0, parse_dates=True)
 data = bt.feeds.PandasData(dataname=df)
-
 strat = ALL_STRATEGIES['run_dmac'](data, commission, sizer)
-print(strat.broker.getvalue())
+"""
