cdab1113e54969bc09155534ddf86c55110e37e4
Ruben <Ruben.kelchtermans9@gmail.com>
2026-02-02 12:02:32 +0100

    made as function

--- utils/results_logger.py
+++ utils/results_logger.py
@@ -1,24 +1,24 @@
 import pandas as pd
 from pathlib import Path
 
- 
-PROJECT_ROOT = Path(__file__).resolve().parents[1]
-SAVE_PATH = PROJECT_ROOT / "database"
+def make_file(): 
+    PROJECT_ROOT = Path(__file__).resolve().parents[1]
+    SAVE_PATH = PROJECT_ROOT / "database"
 
-RESULT_COLUMNS = [
-    "strategy",           # e.g. dmac
-    "symbol",             # AAPL, EURUSD=X
-    "interval",           # 15m, 1h, 1d
-    "parameters",         # dict of strategy parameters
-    "return",             # return in % (15% meaning 15% was made)
-    "sharpe",             # sharpe ratio
-    "trades",             # total number of trades
-    "win_rate",           # percentage of winning trades
-    "start_date",         # pd.Timestamp
-    "end_date",           # pd.Timestamp
-    "commission",         # commission in % per trade
-    "sizer",              # % of capital used per trade
-]
+    RESULT_COLUMNS = [
+        "strategy",           # e.g. dmac
+        "symbol",             # AAPL, EURUSD=X
+        "interval",           # 15m, 1h, 1d
+        "parameters",         # dict of strategy parameters
+        "return",             # return in % (15% meaning 15% was made)
+        "sharpe",             # sharpe ratio
+        "trades",             # total number of trades
+        "win_rate",           # percentage of winning trades
+        "start_date",         # pd.Timestamp
+        "end_date",           # pd.Timestamp
+        "commission",         # commission in % per trade
+        "sizer",              # % of capital used per trade
+    ]
 
-results_df = pd.DataFrame(columns=RESULT_COLUMNS)
-results_df.to_parquet(SAVE_PATH / 'results.parquet', index=True)
+    results_df = pd.DataFrame(columns=RESULT_COLUMNS)
+    results_df.to_parquet(SAVE_PATH / 'results.parquet', index=True)
