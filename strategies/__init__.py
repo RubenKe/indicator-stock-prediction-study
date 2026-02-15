b04b611e1528271e2adeea1afefe751981b48fad
Ruben <Ruben.kelchtermans9@gmail.com>
2026-02-15 21:06:48 +0100

    Added new strategies

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -6,6 +6,11 @@ from .rsi_pullback import run as run_RSI_PULLBACK
 from .atr_volatility_breakout import run as run_ATR_VOL_BO
 from .ma_trend_continuation import run as run_MA_TREND_CONT
 from .vwap_trend_reclaim import run as run_VWAP_RECLAIM
+from .ema_acceleration_breakout import run as run_EMA_ACCEL_BO
+from .hhhl_structure_breakout import run as run_HHHL_STRUCT_BO
+from .ma200_pullback_bounce import run as run_MA200_PULLBACK
+from .inside_bar_breakout_continuation import run as run_INSIDE_BAR_CONT
+from .smc_structure_sweep_ob_fvg import run as run_SMC_SWEEP_OBFVG
 
 ALL_STRATEGIES = {
     "DMAC": run_DMAC,
@@ -16,4 +21,9 @@ ALL_STRATEGIES = {
     "ATR_VOL_BO": run_ATR_VOL_BO,
     "MA_TREND_CONT": run_MA_TREND_CONT,
     "VWAP_RECLAIM": run_VWAP_RECLAIM,
+    "EMA_ACCEL_BO": run_EMA_ACCEL_BO,
+    "HHHL_STRUCT_BO": run_HHHL_STRUCT_BO,
+    "MA200_PULLBACK": run_MA200_PULLBACK,
+    "INSIDE_BAR_CONT": run_INSIDE_BAR_CONT,
+    "SMC_SWEEP_OBFVG": run_SMC_SWEEP_OBFVG,
 }
